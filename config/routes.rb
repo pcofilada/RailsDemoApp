c3d1fd6bfdf03dd7d61796754ac1998f24a93c90
Patrick <pcofilada@gmail.com>
2015-03-23 16:48:21 +0800

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :account_activations, only: [:edit]
-
+  resources :password_resets, only: [:new, :create, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
